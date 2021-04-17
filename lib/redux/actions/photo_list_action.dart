4b18e2ff03b499a0dccb7198700d60da1b9a5a7f
Neko3000 <neko3000@live.com>
2021-04-17 14:07:02 +0800

    refactor(redux/): remove contents of redux

--- lib/redux/actions/photo_list_action.dart
+++
@@ -1,5 +0,0 @@
-import 'package:monotone_flutter/redux/states/photo_list_state.dart';
-
-class PhotoListReloadAction{
-
-}
--- lib/redux/reducers/photo_list_reducer.dart
+++
@@ -1,6 +0,0 @@
-import 'package:monotone_flutter/redux/actions/photo_list_action.dart';
-import 'package:monotone_flutter/redux/states/photo_list_state.dart';
-
-PhotoListState photoListReducer(PhotoListState state, PhotoListReloadAction action){
-  return null;
-}
